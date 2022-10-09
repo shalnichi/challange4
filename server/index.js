05ccac4d2609e70ce61a8ecd51dc8418c5086f06
Faishal Abyan Murtadho <famurtadho@gmail.como>
2022-10-10 01:36:44 +0700

    perf: install package

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-console.log("Implement servermu disini yak 😝!");
+// console.log("Implement servermu disini yak 😝!");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const express = require ("express");
+const { json } = require("express");
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname,"../public/")))
+
+app.get("/", (req, res) => {
+    const letakFile = path.join(__dirname,"../public/index.html");
+    const bacaFile= fs.readFileSync(letakFile,"utf-8");
+    res.send(bacaFile);
+})
+
+app.get("/carimobil", (req, res) => {
+    const letakFile = path.join(__dirname,"../data/cars.json");
+    let bacaFile= fs.readFileSync(letakFile,"utf-8");
+    // bacaFile = JSON.parse(bacaFile);
+    const {tipedriver,tanggal,waktujemput,jumlahpenumpang} = req.query;
+    // const hasil = bacaFile.filter((el)=>{
+    //     return (el.capacity===jumlahpenumpang)&&(el.availableAt===new Date (tanggal+ " " +waktujemput))
+    // })
+    res.send(bacaFile);
+    
+})
+
+app.listen(8080, () => {
+    console.log(`Server Running on port ${8080}`)
+})
